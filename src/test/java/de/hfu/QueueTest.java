4db7e5965c8b4c1914ce755685cf6f4671afff42
Markus <markus.straub22@web.de>
2019-12-19 12:05:45 +0100

    target

--- src/test/java/de/hfu/QueueTest.java
+++ src/test/java/de/hfu/QueueTest.java
@@ -1,36 +1,53 @@
 package de.hfu;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** 
+ * JUnit Test for istErstesHalbjahr
+ */
 
 public class QueueTest {
 
-    @Test
-    public void TestEnqueue(){
-
-    }
-
-    @Test
-    public void TestDequeue(){
-        try {
-            Queue queue = new Queue(0);
-            fail("Falsche Queue Größe!!");
-
-        }catch(IllegalArgumentException e){ }
-
-        Queue queue = new Queue(3);
-        try{
-            queue.dequeue();
-            fail("Nich möglich aus einer leeren Watreschlange ein Elemnt rasuzunehem");
-        }catch(IllegalStateException e){}
-
-
-        for(int i = 1; i<= 4; ++i){
-            queue.enqueue(i);
-        }
-
-        assertEquals("Sind nicht gleich", 1, queue.dequeue());
-
-
-    }
+	@Test
+	public void testEnqueue() {
+		
+		try{
+			
+			Queue TestSchlange1 = new Queue(0); // Falsche Größe
+			fail("Falsche Queue länge");
+			
+		} catch(IllegalArgumentException e){}
+		 
+		Queue TestSchlange2 = new Queue(3);
+		
+		try{
+			
+			TestSchlange2.dequeue(); // Kein Element gefunden
+			fail("Erwartete Ausnahme wurde nicht geworfen");
+			
+		} catch(IllegalStateException e){}
+		
+		TestSchlange2.enqueue(1);
+		TestSchlange2.enqueue(2);
+		TestSchlange2.enqueue(3);
+		
+		assertEquals("Sollte 1 sein", 1, TestSchlange2.queue[0]);
+		assertEquals("Sollte 2 sein", 2, TestSchlange2.queue[1]);
+		assertEquals("Sollte 3 sein", 3, TestSchlange2.queue[2]);
+		
+		
+		TestSchlange2.enqueue(1);
+		TestSchlange2.enqueue(2);
+		TestSchlange2.enqueue(3);
+		TestSchlange2.enqueue(4);
+		
+		assertEquals("Sollte 1 sein (zweites mal)", 1, TestSchlange2.dequeue());
+		assertEquals("Sollte 2 sein (zweites mal)", 2, TestSchlange2.dequeue());
+		assertEquals("Sollte 4 sein (zweites mal)", 4, TestSchlange2.dequeue());
+		
+	
+	}
+			
 }
--- src/test/java/de/hfu/UtilTest.java
+++ src/test/java/de/hfu/UtilTest.java
@@ -1,17 +1,26 @@
 package de.hfu;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+/** 
+ * JUnit Test for istErstesHalbjahr
+ */
+
 public class UtilTest {
+	
+	@Test
+	public void shouldbefirsthalf_year(){
+		
+		int Monat = 6;
+		assertTrue("Soll richtig sein", Util.istErstesHalbjahr(Monat));
+		int Monat2 = 7;
+		assertFalse("Soll falsch sein", Util.istErstesHalbjahr(Monat2));
+		int Monat3 = 5;
+		assertTrue("Soll richtig sein", Util.istErstesHalbjahr(Monat3));
+		
+	}
+	
 
-    @Test
-    public void TestistErstesHalbjahr(){
-        int monat = 6;
-        int monat1 = 7;
-        assertTrue("Ist nicht erstes Halbjahr", Util.istErstesHalbjahr(monat));
-        assertFalse("Ist zweites Halbjahr", Util.istErstesHalbjahr(monat1));
-
-
-    }
 }
